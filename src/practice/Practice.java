2e47cb34b9a55856e7fb71314669f525b5fbbf07
ijaskaranjohal <156921792+ijaskaranjohal@users.noreply.github.com>
2024-02-05 14:16:35 -0500

    Update Practice.java

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -16,6 +16,7 @@ public class Practice {
     public static void main(String[] args) {
         // TODO code application logic here
         //my first change
+        //my second change
     }
     
 }
